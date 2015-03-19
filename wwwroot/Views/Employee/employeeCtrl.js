87c9a2ef1841630ba89dcca67e4d95afdad08994
jgli <jgli@pcgus.com>
2015-03-19 15:52:50 -0400

    employee detail with style

--- wwwroot/Views/Employee/employeeCtrl.js
+++ wwwroot/Views/Employee/employeeCtrl.js
@@ -3,6 +3,8 @@
 
     function employeeCtrl(repositoryService, dbEntityService) {
         var vm = this;
+        vm.showDetail = false;
+        vm.isFromBack = false;
 
         repositoryService.getDataList(dbEntityService.entities.employee).then(function (data) {
             vm.employeeList = data;
@@ -10,9 +12,14 @@
 
         vm.employeeDetail = function (id) {
             vm.employee = _.find(vm.employeeList, function (e) {
+                vm.showDetail = true;
                 return e.EmployeeID === id;
             });
-            console.log(vm.employee.EmployeeID);
+        }
+
+        vm.backToList = function () {
+            vm.isFromBack = true;
+            vm.showDetail = false;
         }
     }
 
