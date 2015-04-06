2f7c76b374dbdfcbc8c112e8608ed8dcaa1dffac
Rain <ljghot@hotmail.com>
2015-04-06 17:36:57 -0400

    rain grid page size 15

--- wwwroot/Directives/RainGrid/rainGrid.js
+++ wwwroot/Directives/RainGrid/rainGrid.js
@@ -69,6 +69,7 @@
                 $scope.pageSizes = [
                     {label: ' 5', value: 5},
                     {label: '10', value: 10},
+                    {label: '15', value: 15},
                     {label: '20', value: 20}
                 ];
                 $scope.pageSize = $scope.pageSizes[1];
