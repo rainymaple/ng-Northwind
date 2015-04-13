61c97b5be11fcde0fd761e2193ff450657ef2791
jgli <jgli@pcgus.com>
2015-04-13 15:53:31 -0400

    sass

--- wwwroot/Directives/RainCheckbox/rainCheckbox.js
+++ wwwroot/Directives/RainCheckbox/rainCheckbox.js
@@ -23,13 +23,15 @@
     }
 
     function getTemplate() {
-        return '<div class="clearfix" style="display:table;margin: 0 auto;padding: 3px 10px 0 0;">' +
-            '<div class="pull-right" style="font-size: 16px;font-weight: bold;margin-left: 5px;">' +
+        return '<div class="rain-checkbox-container">' +
+            '<div class="checkbox-image">'+
+            '<div  class="rain-checkbox">' +
+            '<input type="checkbox" ng-model="rainCheckbox" style="display: inline;"/>' +
+            '<label class="checkbox-label" ng-click="onclick()"></label>' +
+            '</div>' +
+            '</div>' +
+            '<div class="checkbox-text">' +
             '{{text}}</div>' +
-            '<div class="pull-right">'+
-            '<div style="margin:0 auto" class="rain-checkbox">' +
-            '<input type="checkbox" ng-model="rainCheckbox" style="width: 0px;"/>' +
-            '<label class="checkbox-label" style="margin-top: 0" ng-click="onclick()"></label>' +
-            '</div></div></div>';
+            '</div>';
     }
 })(angular.module('appNorthwind'));
